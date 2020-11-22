e53052945c8a3f26c934a82e0d02a6b0bc0abd6e
ArthurMayet <arthur.mayettissot@gmail.com>
2020-11-22 17:01:59 +0100

    Add EC2 monitoring

--- Aws/README.md
+++ Aws/README.md
@@ -4,3 +4,9 @@ Demo project to manage EC2 instances threw boto3 and click
 
 arthur use the configuration file created by aws cli e.g
 `aws config --profile arthur`
+
+## Running
+
+`ipenv run python Formation/Aws/click_boto.py <Command> <--project=PROJECT>`
+*Command* is list, start or stop
+*project* is optionnal
--- Aws/click_boto.py
+++ Aws/click_boto.py
@@ -4,18 +4,83 @@ import click
 session = boto3.Session(profile_name='arthur')
 ec2 = session.resource('ec2')
 
-@click.command()
-def list_instances():
-    for i in ec2.instances.all():
+def filter_instances(project):
+
+    instances = []
+    if project:
+        filters = [{'Name': 'tag:Project', 'Values': [project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    return instances
+
+@click.group()
+def instances():
+    """ Commands for instances """
+@instances.command('list')
+@click.option('--project', default=None,
+              help="Only instances for project (tag Project:<name>)")
+
+def list_instances(project):
+    "List EC2 Instances"
+    instances = filter_instances(project)
+
+    for instance in instances:
+        tags = {t['Key']: t['Value'] for t in instance.tags or []}
         print(', '.join((
-            i.id,
-            i.instance_type,
-            i.placement['AvailabilityZone'],
-            i.state['Name'],
-            i.public_dns_name)))
+            instance.id,
+            instance.instance_type,
+            instance.placement['AvailabilityZone'],
+            instance.state['Name'],
+            instance.public_dns_name,
+            tags.get('Project', '<no_project>')
+        )))
 
     return
 
+@instances.command('stop')
+@click.option('--project', default=None,
+              help="Only instances for project (tag Project:<name>)")
+def stop_instances(project):
+    "Stop EC2 Instances"
+    instances = filter_instances(project)
+
+    for instance in instances:
+        tags = {t['Key']: t['Value'] for t in instance.tags or []}
+        print("Stopping {0} instance ....".format(instance.id))
+        instance.stop()
+        print(', '.join((
+            instance.id,
+            instance.instance_type,
+            instance.placement['AvailabilityZone'],
+            instance.state['Name'],
+            tags.get('Project', '<no_project>')
+        )))
+
+    return
+
+@instances.command('start')
+@click.option('--project', default=None,
+              help="Only instances for project (tag Project:<name>)")
+def start_instances(project):
+    "Start EC2 Instances"
+    instances = filter_instances(project)
+
+    for instance in instances:
+        tags = {t['Key']: t['Value'] for t in instance.tags or []}
+        print("Starting {0} instance ....".format(instance.id))
+        instance.start()
+        print(', '.join((
+            instance.id,
+            instance.instance_type,
+            instance.placement['AvailabilityZone'],
+            instance.state['Name'],
+            instance.public_dns_name,
+            tags.get('Project', '<no_project>')
+        )))
+
+    return
 
 if __name__ == '__main__':
-    list_instances()
+    instances()
